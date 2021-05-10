5ea47a1c46fda99ab584090bfcf71939309cd583
Leon Bahovec <leon.bahovec@student.fmf.uni-lj.si>
2021-05-10 19:00:17 +0200

    dodani piškotki

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 STEVILO_DOVOLJENIH_NAPAK = 10
 
@@ -9,13 +10,15 @@ NAPACNA_CRKA = "-"
 ZMAGA = "W"
 PORAZ = "X"
 ZACETEK = "A"
+DATOTEKA_S_STANJEM = "podatki.json"
 
 class Vislice:
     """
     Krovni objekt, ki upravlja z vsemi igrami
     """
-    def __init__(self):
-        self.igre = {}
+    def __init__(self, zacetne_igre, datoteka_s_stanjem=DATOTEKA_S_STANJEM):
+        self.igre = zacetne_igre
+        self.datoteka_s_stanjem = datoteka_s_stanjem
     
     def prost_id_igre(self):
         if not self.igre:
@@ -28,10 +31,56 @@ class Vislice:
         sveza = nova_igra()
         self.igre[nov_id] = (sveza, ZACETEK)
         return nov_id
+   
     def ugibaj(self, id_igre, crka):
         igra, stanje = self.igre[id_igre]
         novo_stanje = igra.ugibaj(crka)
         self.igre[id_igre] = (igra, novo_stanje)
+    
+    def dobi_json_slovar(self):
+        slovar_iger = {}
+        for (id_igre, (igra, stanje)) in self.igre.items():
+            slovar_iger[id_igre] = [
+                igra.dobi_json_slovar(), #Kličem serializacijo igre
+                stanje,
+            ]
+        return {
+            "igre": slovar_iger,
+            "datoteka_s_stanjem": self.datoteka_s_stanjem,
+        }
+    
+    @staticmethod
+    def preberi_iz_datoteke(ime_datoteke="podatki.json"):
+        with open(ime_datoteke, "r", encoding="utf-8") as in_file:
+            slovar = json.load(in_file) # Slovar
+        return Vislice.dobi_vislice_iz_slovarja(slovar)
+
+    @staticmethod
+    def dobi_vislice_iz_slovarja(slovar):
+        slovar_iger = {} # To je slovar objektov "Igra"
+        for id_igre, (igra_slovar, stanje) in slovar["igre"].items():
+            slovar_iger[int(id_igre)] = (
+                Igra.dobi_igro_iz_slovarja(igra_slovar),
+                stanje
+            )
+        return Vislice(slovar_iger, slovar["datoteka_s_stanjem"])
+
+
+
+    def zapisi_v_datoteko(self):
+        # Naredi slovar
+        slovar = self.dobi_json_slovar()
+        # Zapiši ga v datoteko
+        with open(self.datoteka_s_stanjem, "w", encoding="utf-8") as out_file:
+            json.dump(slovar, out_file)
+    
+    
+    
+    
+
+
+
+
 
 
 class Igra:
@@ -42,7 +91,19 @@ class Igra:
             self.crke = []
         else:
             self.crke = crke
+    
+    def dobi_json_slovar(self):
+        return {
+            "geslo": self.geslo,
+            "crke": self.crke,
+        }
+    
+    @staticmethod
+    def dobi_igro_iz_slovarja(slovar):
+        return Igra(
+            slovar["geslo"], slovar.get("crke",""),      #.get metoda vrne vrednost slovarja, ce obstaja tak kljuc, sicer pa 2. vrednost, ki jo podamo metodi .get
 
+        )
     def napacne_crke(self):
         return [crka for crka in self.crke if crka not in self.geslo]
 
--- vislice.py
+++ vislice.py
@@ -1,30 +1,62 @@
 import bottle
 import model
 
-vislice = model.Vislice()
+COOKIE_ID_IGRE = "ID_IGRE"
+SECRET = "SECRET"
 
 
 @bottle.get("/")
-def osnovna_stran():
+def index():
     return bottle.template("index.tpl")
 
 
 @bottle.get("/igra/")
 def nova_igra():
-    id_igre = vislice.nova_igra()
-    return bottle.redirect(f"/igra/{id_igre}/")
+    vislice = model.Vislice.preberi_iz_datoteke()
 
+    id_igre = vislice.nova_igra()
+    novi_url = f"/igra/{id_igre}/"
+    
+    vislice.zapisi_v_datoteko()
+    
+    bottle.response.set_cookie(COOKIE_ID_IGRE, id_igre, path="/", secret=SECRET)
+    
+    bottle.redirect("/igraj/")
+
+@bottle.get("/igraj/")
+def igraj_igro():
+    id_igre = int(bottle.request.get_cookie(COOKIE_ID_IGRE, secret=SECRET))
+    
+    vislice = model.Vislice.preberi_iz_datoteke()
+    igra, stanje = vislice.igre[id_igre]
+    
+    vislice.zapisi_v_datoteko()
+    return bottle.template("igra.tpl", igra=igra, stanje=stanje)
+@bottle.post("/igraj/")
+def ugibaj_crko_igraj():
+    id_igre = int(bottle.request.get_cookie(COOKIE_ID_IGRE, secret=SECRET))
+    vislice = model.Vislice.preberi_iz_datoteke()
+    ugibana_crka = bottle.request.forms["ugibana_crka"]
+    vislice.ugibaj(id_igre, ugibana_crka)
+    vislice.zapisi_v_datoteko()
+    return bottle.redirect(f"/igraj/")
 
 @bottle.get("/igra/<id_igre:int>/")
 def poglej_igro(id_igre):
+    id_igre = int(bottle.request.get_cookie(COOKIE_ID_IGRE, secret=SECRET))
+    vislice = model.Vislice.preberi_iz_datoteke()
     igra, stanje = vislice.igre[id_igre]
+    vislice.zapisi_v_datoteko()
     return bottle.template("igra.tpl", igra=igra, stanje=stanje)
 
 @bottle.post("/igra/<id_igre:int>/")
 def ugibaj_crko(id_igre):
+    vislice = model.Vislice.preberi_iz_datoteke()
     ugibana_crka = bottle.request.forms["ugibana_crka"]
     vislice.ugibaj(id_igre, ugibana_crka)
-    bottle.redirect("")
+    vislice.zapisi_v_datoteko()
+    return bottle.redirect(f"/igra/{id_igre}/")
+
 
 #Poženemo celotno zadevo
 bottle.run(reloader=True, debug=True)
